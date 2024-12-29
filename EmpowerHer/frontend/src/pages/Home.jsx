e9e501d78e0dbb5c6ef85fbea4c9690c8d8c4793
Muhammad Owais Khan <Mokay5226@gmail.com>
2024-12-29 17:42:52 +0500

    Ye kaaam poora krne k baad bug ajata

--- EmpowerHer/frontend/src/pages/Home.jsx
+++ EmpowerHer/frontend/src/pages/Home.jsx
@@ -122,7 +122,7 @@ const PublicHome = () => {
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
                     <Link
-                      to="/signup"
+                      to="/Register"
                       className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 md:py-4 md:text-lg md:px-10"
                     >
                       Get started
